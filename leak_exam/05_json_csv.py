9f14eccde18eb50a18f718b88ab4e9fd092da196
Sanchit Bishwakarma <iamjoblessgod@gmail.com>
2026-06-01 22:25:01 +0545

    Add functionality to read student data from CSV and save top students to JSON

--- leak_exam/05_json_csv.py
+++ leak_exam/05_json_csv.py
@@ -2,6 +2,9 @@ import csv
 import json
 
 filePath = "./db/students.csv"
+topStudentFilePath = "./db/top_students.json"
+
+top_students = []
 
 with open(filePath, "r") as file:
     reader = csv.reader(file)
@@ -9,4 +12,11 @@ with open(filePath, "r") as file:
         name, age, score = row
         if int(score) > 80:
             print(f"Student: {name}")
-       
+            top_students.append({
+                "Name": name,
+                "Age": int(age),
+                "Score": int(score)
+            })
+            
+with open(topStudentFilePath, "w") as file:
+    json.dump(top_students, file, indent=4)
