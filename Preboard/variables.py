c68952e1b2bcecd448cd457275509fd769143bc5
Sanchit Bishwakarma <iamjoblessgod@gmail.com>
2026-07-26 00:00:00 +0545

    html content done

---
+++ Preboard/variables.py
@@ -0,0 +1,44 @@
+# list
+a_list = [1, 2, 3, 4, 5]
+
+# dictionary
+b_dict = {"name": "Sanchit", "age": 20, "city": "Kathmandu"}
+
+# tuple
+c_tuple = (1, 2, 3, 4, 5)
+
+# set
+d_set = {1, 2, 3, 4, 5}
+
+# string
+e_string = "Hello, Boss!"
+
+# boolean
+f_boolean = True
+
+# integer
+g_integer = 43
+
+# float
+h_float = 3.14
+
+# complex
+i_complex = 2 + 3j
+
+# bytes
+j_bytes = b"Hello, World!"
+
+print("List:", a_list)
+print("Dictionary:", b_dict)
+print("Tuple:", c_tuple)
+print("Set:", d_set)
+print("String:", e_string)
+print("Boolean:", f_boolean)
+print("Integer:", g_integer)
+print("Float:", h_float)
+print("Complex:", i_complex)
+print("Bytes:", j_bytes)Commit at 2026-07-26T00:00:00
+Commit at 2026-07-27T00:00:00
+Commit at 2026-07-28T00:00:00
+Commit at 2026-07-29T00:00:00
+Commit at 2026-07-30T00:00:00
--- mr-beast-python/main.py
+++ mr-beast-python/main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 # take the max number from user
 maxNum = int(input("Enter the max number: "))
 
+
 # files numbers
 # maxFileNum = int(input("Enter the max file: number: "))
 
@@ -13,6 +14,7 @@ time1 = datetime.now()
 
 
 for n in range(maxNum):
+    print(f"[Info] Creating \'{FILE_NAME}_{n+1}{FILE_EXT}\' file...")
     with open(f"store/{FILE_NAME}_{n+1}{FILE_EXT}", "w") as file:
         file.write(str(n + 1))
 
@@ -21,4 +23,5 @@ time2 = datetime.now()
 
 duration = time2 - time1
 
-print(f"\n[Time] {duration}\n")
+print(f"\n[Time] {duration.seconds // 3600}h {duration.seconds // 60}m {duration.seconds % 60}s {duration.microseconds}ms\n")
+Commit at 2026-07-29T09:42:27
